2936988c6e7b48666fb59aa8aea3e0e60ad4eccd
João Lucas <joaolucasmm2512@gmail.com>
2025-05-09 20:24:08 -0300

    first commit

---
+++ src/main/java/java10x/cadastrosdeninjas/CadastrosDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package java10x.cadastrosdeninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosDeNinjasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastrosDeNinjasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/java10x/cadastrosdeninjas/CadastrosDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package java10x.cadastrosdeninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosDeNinjasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
